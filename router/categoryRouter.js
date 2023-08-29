0607087e21e3786c6e70c7897297805cc47ed8a8
JahnwiTiwary <jahnwitiwary@gmail.com>
2023-08-29 21:23:44 +0530

    Changes in categoryRouter

--- router/categoryRouter.js
+++ router/categoryRouter.js
@@ -14,7 +14,9 @@ router.post("/event/category", [Auth.verifyToken], async (req, res) => {
 
     if (existingCategory) {
       // If a category with the same name exists, return an error response
-      return res.status(400).json({ message: "Category already exists" });
+     
+      //return res.status(400).json({ message: "Category already exists" });
+        return res.send({status : false, message: "Category already exists"});
     }
 
     // If no existing category found, proceed to add the new category
@@ -23,7 +25,8 @@ router.post("/event/category", [Auth.verifyToken], async (req, res) => {
 
     res.status(201).json(createdCategory);
   } catch (error) {
-    res.status(500).json({ message: "Internal server error" });
+    //res.status(500).json({ message: "Internal server error" });
+      return res.send({status: false, message: "Internal server error"});
   }
 });
 
@@ -31,9 +34,9 @@ router.post("/event/category", [Auth.verifyToken], async (req, res) => {
 router.get("/category", async (req, res) => {
   try {
     const getCategory = await Category.find({});
-    res.send(getCategory);
+    res.send({status: true, data: getCategory});
   } catch (e) {
-    res.status(400).send(e);
+    res.send({status:false, message: e});
   }
 });
 
@@ -42,22 +45,40 @@ router.get("/category/:category_name", async (req, res) => {
   try {
     const category_name = req.params.category_name;
     const getCategory = await Category.find({ category_name: category_name });
-    res.send(getCategory);
-  } catch (e) {
-    res.status(400).send(e);
+    
+     //console.log(typeof(getCategory));
+     //console.log(getCategory.length);
+    
+    if(getCategory.length>0){
+     res.send({status: true, data: getCategory});
+     }
+
+    else{
+      //console.log("hii");
+     res.send({status : false, message: "Category does not exist"});
+     }
+  } 
+  catch (e) {
+    res.send({status:false, message: e});
   }
 });
 
 //DELETE category
 router.delete("/category/:category_name", Auth.verifyToken, async (req, res) => {
   try {
-    console.log("inside delete");
+    //console.log("inside delete");
     const category_name = req.params.category_name;
     const delCategory = await Category.findOneAndDelete({category_name: category_name});
-    res.send(delCategory);
-    console.log("Success");
+    
+    if(delCategory){
+    res.send({status: true, data: delCategory});
+    }
+    else{
+      res.send({status:false, message:"Category does not exist"});
+    }
+    //console.log("Success");
   } catch (e) {
-    res.status(500).send(e);
+    res.send({status: false, message: e});
   }
 });
 
